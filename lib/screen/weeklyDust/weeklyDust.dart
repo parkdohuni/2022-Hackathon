d1e0e76230069684d933f286455c5e0455edc270
HeewonP825 <80496838+HeewonP825@users.noreply.github.com>
2022-09-22 17:14:43 +0900

    change theme color

--- lib/screen/weeklyDust/weeklyDust.dart
+++ lib/screen/weeklyDust/weeklyDust.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class WeeklyDust extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Center(
-          child: Text("NoKidsZoneCafe Screen"),
-        )
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/icons/logo.png',
+                fit: BoxFit.contain,
+                height: 165,
+              ),
+            ],
+          ),
+        ),
+      body: Container(
+          child: Center(
+            child: Text("RecommandPlace Screen"),
+          )
+      ),
     );
   }
 }
