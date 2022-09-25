98e30028249c80f0e9cbefcd246f6cf1a9a63194
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2022-09-25 15:42:45 +0900

    update navitems

--- lib/model/navItem.dart
+++ lib/model/navItem.dart
@@ -8,6 +8,6 @@ class NavItem {
 
 List<NavItem> navItems = [
   NavItem(id: 0, label: "미세먼지", icon: "assets/icons/face-mask-svgrepo-com.svg"),
-  NavItem(id: 1, label: "추천장소", icon: "assets/icons/location.png"),
-  NavItem(id: 2, label: "예스키즈존", icon: "assets/icons/baby-boy.png"),
+  NavItem(id: 1, label: "추천장소", icon: "assets/icons/location.svg"),
+  NavItem(id: 2, label: "예스키즈존", icon: "assets/icons/ok-svgrepo-com.svg"),
 ];
